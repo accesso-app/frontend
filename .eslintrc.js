ebc39131279ba66a1bb9cf4a4662af8fd4d4a477
risenforces <risenforces@gmail.com>
2021-05-22 22:45:08 +0500

    chore(config): set up eslint-kit

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+require('@eslint-kit/eslint-config-patch');
+
+module.exports = {
+  extends: [
+    '@eslint-kit/base',
+    '@eslint-kit/typescript',
+    '@eslint-kit/node',
+    '@eslint-kit/react',
+    '@eslint-kit/prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'import/no-unresolved': 'off',
+  },
+};
