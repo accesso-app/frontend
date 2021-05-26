b133d3f5e7824d05f89be73edbd94af4e26c5aa9
risenforces <risenforces@gmail.com>
2021-05-26 16:43:35 +0500

    refactor(api): completely replace resources with generated api

--- src/api/request/index.ts
+++ src/api/request/index.ts
@@ -6,7 +6,6 @@ export {
 } from './common';
 
 export type { Answer } from './common';
-export { createResource } from './resource';
 
 if (process.env.BUILD_TARGET === 'server') {
   require('./server');
--- src/pages/register/confirm/model.ts
+++ src/pages/register/confirm/model.ts
@@ -6,10 +6,15 @@ import {
   guard,
   sample,
 } from 'effector-root';
-import { registerConfirmation } from 'api/register';
+import * as typed from 'typed-contracts';
+import { registerConfirmation, registerConfirmationBadRequest } from 'api';
 
 import { checkAnonymous } from 'features/session';
 import { createStart } from 'lib/page-routing';
+
+type BadRequestStatus = typed.Get<
+  typeof registerConfirmationBadRequest
+>['error'];
 
 export const pageStart = createStart();
 const codeReceived = pageStart.filterMap(({ params }) => params.code);
@@ -22,14 +27,7 @@ export const repeatChanged = createEvent<ChangeEvent<HTMLInputElement>>();
 export const $isFormPending = registerConfirmation.pending;
 
 export const $isRegistrationFinished = createStore(false);
-export const $failure =
-  createStore<
-    | null
-    | 'code_invalid_or_expired'
-    | 'email_already_activated'
-    | 'invalid_form'
-    | 'invalid_payload'
-  >(null);
+export const $failure = createStore<null | BadRequestStatus>(null);
 
 export const $code = createStore('');
 
@@ -84,10 +82,20 @@ $repeat.on(repeatChanged, (_, event) => event.currentTarget.value);
 $failure
   .on(pageReady, () => null)
   .on(registerConfirmation, () => null)
-  .on(registerConfirmation.failBody, (_, { error }) => error);
+  .on(registerConfirmation.failData, (_, failure) => {
+    if (failure.status !== 'bad_request') {
+      return null;
+    }
+
+    return failure.error.error;
+  });
 
 guard({
-  source: sample($form, formSubmitted),
+  source: sample({
+    source: $form,
+    clock: formSubmitted,
+    fn: (body) => ({ body }),
+  }),
   filter: $isSubmitDisabled.map((is) => !is),
   target: registerConfirmation,
 });
